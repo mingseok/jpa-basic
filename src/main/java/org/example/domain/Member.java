000fc8add0d9236eef52b7eddcf3c30e04033b68
minseok <alstjr706@naver.com>
2023-05-16 23:35:45 +0900

    refactor : (JPA) Member 엔티티 코드 수정

--- src/main/java/org/example/domain/Member.java
+++ src/main/java/org/example/domain/Member.java
@@ -7,55 +7,12 @@ public class Member {
 
     @Id
     @GeneratedValue
-    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(length = 10)
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
 
-    private String city;
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 
-    private String street;
-
-    private String zipcode;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
 }
