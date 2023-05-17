0ac9a20d7a9f37be35ff667fb176263c1ef5e48b
minseok <alstjr706@naver.com>
2023-05-18 00:52:19 +0900

    refactor : (JPA) 일대일 연관관계 매핑

--- src/main/java/org/example/domain/Member.java
+++ src/main/java/org/example/domain/Member.java
@@ -1,14 +1,11 @@
 package org.example.domain;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Member {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private Long id;
 
     @Column(name = "USERNAME")
@@ -18,33 +15,8 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-
-        // 여기부분
-        team.getMembers().add(this); // 이렇게하면 양쪽으로 세팅이 끝난 것이다.
-    }
 }
