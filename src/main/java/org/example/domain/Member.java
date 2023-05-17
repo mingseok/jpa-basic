e5a9ffa3ffcd5c510214486f51ef07809f756d73
minseok <alstjr706@naver.com>
2023-05-17 15:05:44 +0900

    feat : (JPA) 연관관계 매핑

--- src/main/java/org/example/domain/Member.java
+++ src/main/java/org/example/domain/Member.java
@@ -12,7 +12,37 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+
+        // 여기부분
+        team.getMembers().add(this); // 이렇게하면 양쪽으로 세팅이 끝난 것이다.
+    }
 }
--- src/main/java/org/example/domain/Team.java
+++ src/main/java/org/example/domain/Team.java
@@ -1,9 +1,8 @@
 package org.example.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -11,11 +10,32 @@ public class Team {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
+    private String name;
 
-    @Column(name = "USERNAME")
-    private String username;
+    @OneToMany(mappedBy = "team") // 괄호안에 들어가 있는것은 뭐냐? -> "team으로 매핑이 되어 있는 상태이다" 라고 말하는 것이다.
+    private List<Member> members = new ArrayList<>(); // 이렇게 선언하는것이 관례이다.
 
-    @Column(name = "TEAM_ID")
-    private String teamId;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
